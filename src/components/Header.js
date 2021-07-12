1cf8949ec78d5c3715662df16b5624a100575692
Evgeny-ExperaHub <evgeny@experahub.com>
2021-07-12 21:56:50 +0300

    Header Completed

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Typed from "react-typed"
+
+const Header = () => {
+    return (
+   <div className="header-wraper">
+       <div className="main-info">
+           <h1>Web development</h1>
+           <Typed 
+           className="typed-text"
+           strings={[ 
+               "FullStack Development", 
+               "FronEnd Development", 
+               "BackEnd Development"]}
+               typeSpeed={40}
+               backSpeed={50}  
+               loop
+               />
+                 <a href="#" className="btn-main-offer">contact me</a>
+       </div>
+   </div>
+    )
+}
+
+export default Header
